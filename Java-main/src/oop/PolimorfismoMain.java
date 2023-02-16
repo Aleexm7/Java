0629239999a97c9ff7238bfdc4f225aecf131a29
Aleexm7 <alejandromunozdominguez@gmail.com>
2023-02-16 18:14:10 +0100

    Nuevos ejercicios oop

---
+++ Java-main/src/oop/PolimorfismoMain.java
@@ -0,0 +1,33 @@
+package oop;
+
+public class PolimorfismoMain {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Coche coche1 = new Coche();
+		
+		CocheElectrico coche2 = new CocheElectrico();
+		
+		
+		//POLIMORFISMO
+		
+		//El polimorfismo se utiliza cuando una clase es heredada de otra, podemos crear objetos de la siguiente manera:
+
+		Coche coche4 = new CocheElectrico();
+		
+		Coche coche5 = new CocheHibrido();
+		
+		
+		if(coche4 instanceof CocheElectrico) { //Con la funcion instanceof nos permite comprobar si este objeto es instancia de una clase
+			System.out.println("Es un coche electrico");
+		}
+		
+		if(coche4 instanceof CocheHibrido) {
+			System.out.println("Es un coche Hibrido");
+		}
+	}
+
+}
