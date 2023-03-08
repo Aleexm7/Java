035cff4b4b1f1049c1d10b114c9fdea0e3458726
Aleexm7 <alejandromunozdominguez@gmail.com>
2023-03-08 14:03:36 +0100

    new changes in files

--- Java-main/src/introduccion/Arrays_bidimensionales.java
+++ Java-main/src/introduccion/Arrays_bidimensionales.java
@@ -5,6 +5,7 @@ public class Arrays_bidimensionales {
 	public static void main(String[] args) {
 		
 		
+		//ARRAY BIDIMENSIONAL 1
 		int[][] matrix = {
 				{10,5,16,20},
 				{34,60,30,50},
@@ -21,6 +22,32 @@ public class Arrays_bidimensionales {
 			}
 		}
 		
+		//ARRAY BIDIMENSIONAL 2
+	
 		
+		int arrayBidi[][] = new int[2][4]; //El primer elemento son las filas y el segundo las columnas
+		
+		arrayBidi[0][0] = 1;
+		arrayBidi[0][1] = 2;
+		arrayBidi[0][2] = 3;
+		arrayBidi[0][3] = 4;
+		
+		arrayBidi[1][0] = 10;
+		arrayBidi[1][1] = 20;
+		arrayBidi[1][2] = 30;
+		arrayBidi[1][3] = 40;
+		
+		
+		for(int i = 0; i < arrayBidi.length; i++) {
+			
+			System.out.println("Valor de i : " + i); //Accedemos a las filas con este for
+		
+		
+		for (int j = 0; j < arrayBidi[1].length; j ++ ) { //Iteramos sobre las filas 
+			
+			System.out.println("Estoy en  i = " + i + " , j =" + j);
+			System.out.println(arrayBidi[i][j]);
+		}
 	}
+}
 }
